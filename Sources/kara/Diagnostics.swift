dff2fc9c47a496ced9b4c399c02b39e15b920487
Max Desiatov <max@desiatov.com>
2021-08-11 12:12:22 +0100

    Implement `StatefulParser`, add HKT fixture

--- Sources/kara/Diagnostics.swift
+++ Sources/kara/Diagnostics.swift
@@ -4,25 +4,9 @@
 
 import Parsing
 
-extension Parsers {
-    struct ConsumedCount<P: Parser>: Parser where P.Input: Collection {
-        init(parser: P) {
-            self.parser = parser
-        }
+struct SourceLocation<Element> {
+    let start: String.Index
+    let end: String.Index
 
-        let parser: P
-
-        func parse(_ input: inout P.Input) -> (P.Output, Int)? {
-            let original = input
-            let result = parser.parse(&input)
-
-            return result.map { ($0, original.count - input.count) }
-        }
-    }
-}
-
-extension Parser where Input: Collection {
-    func consumedCount() -> Parsers.ConsumedCount<Self> {
-        .init(parser: self)
-    }
+    let element: Element
 }
