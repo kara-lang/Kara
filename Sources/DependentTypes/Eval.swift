99661bfc63965ca227e9f706c85200cfa64e94e6
Max Desiatov <max@desiatov.com>
2021-08-23 21:33:59 +0100

    Fully implement Eval in DependentTypes

--- Sources/DependentTypes/Eval.swift
+++ Sources/DependentTypes/Eval.swift
@@ -99,6 +99,97 @@ extension Inferred {
       }
 
       return rec(n.eval(names, environment))
+
+    case let .vec(a, n):
+      return .vec(a.eval(names, environment), n.eval(names, environment))
+
+    case let .vecElim(a, m, mn, mc, n, xs):
+      let mnVal = mn.eval(names, environment)
+      let mcVal = mc.eval(names, environment)
+
+      func rec(_ nVal: Value, _ xsVal: Value) -> Value {
+        switch xsVal {
+        case .nil:
+          return mnVal
+
+        case let .cons(_, k, x, xs):
+          return [k, x, xs, rec(k, xs)].reduce(mcVal) { $0.apply($1) }
+
+        case let .neutral(n):
+          return .neutral(
+            .vecElim(
+              a.eval(names, environment),
+              m.eval(names, environment),
+              mnVal,
+              mcVal,
+              nVal,
+              n
+            )
+          )
+        default:
+          fatalError()
+        }
+      }
+
+      return rec(n.eval(names, environment), xs.eval(names, environment))
+
+    case let .eq(a, x, y):
+      return .eq(a.eval(names, environment), x.eval(names, environment), y.eval(names, environment))
+
+    case let .eqElim(a, m, mr, x, y, eq):
+      let mrVal = mr.eval(names, environment)
+
+      switch eq.eval(names, environment) {
+      case let .refl(_, z):
+        return mrVal.apply(z)
+
+      case let .neutral(n):
+        return .neutral(
+          .eqElim(
+            a.eval(names, environment),
+            m.eval(names, environment),
+            mrVal,
+            x.eval(names, environment),
+            y.eval(names, environment),
+            n
+          )
+        )
+      default:
+        fatalError()
+      }
+
+    case let .fin(n):
+      return .fin(n.eval(names, environment))
+
+    case let .finElim(m, mz, ms, n, f):
+      let mzVal = mz.eval(names, environment)
+      let msVal = ms.eval(names, environment)
+
+      func rec(_ fVal: Value) -> Value {
+        switch fVal {
+        case let .fZero(k):
+          return mzVal.apply(k)
+
+        case let .fSucc(k, g):
+          return [k, g, rec(g)].reduce(msVal) { $0.apply($1) }
+
+        case let .neutral(n1):
+          return .neutral(
+            .finElim(
+              m.eval(names, environment),
+              mz.eval(names, environment),
+              ms.eval(names, environment),
+              n.eval(names, environment),
+              n1
+            )
+          )
+
+        default:
+          fatalError()
+        }
+      }
+
+      return rec(f.eval(names, environment))
     }
   }
 }
